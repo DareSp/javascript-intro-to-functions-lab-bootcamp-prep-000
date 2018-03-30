402cccf5a280ba9e2dedbf6bb6c7c733d0d1cfe1
Jody Hestick <jhestick@gmail.com>
2018-03-30 23:24:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,9 +17,10 @@ function logWhisper(string){
 function sayHiToGrandma(string){
   if (string === string.toLowerCase){
     return "I can't hear you!"
-  } 
-  if (string === string.toUpperCase){
+  } else if (string === string.toUpperCase){
     return "YES INDEED!"
+  } else {
+    return "Error"
   }
 }
 
